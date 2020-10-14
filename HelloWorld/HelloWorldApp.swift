45c1d67ad3c2c87cbb7e5433547002e93484e31b
Rooparsh <rooparshkalia@gmail.com>
2020-10-14 15:03:55 +0530

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Rooparsh Kalia on 14/10/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
